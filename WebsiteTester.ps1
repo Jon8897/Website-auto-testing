9e00237454d7fd403f10825f692816e1df5a0e53
jon8897 <jontykeefe8@gmail.com>
2023-04-25 15:43:21 +0100

    Connecting GUI has and error

--- WebsiteTester.ps1
+++ WebsiteTester.ps1
@@ -1,79 +1,101 @@
-# Import Selenium module
-Import-Module Selenium
+# Load GUI from XAML
+Add-Type -AssemblyName PresentationFramework
 
-# Start new web driver
-try {
-    $chromeOptions = New-Object OpenQA.Selenium.Chrome.ChromeOptions
-    $chromeOptions.AcceptInsecureCertificates = $true
-    $driver = Start-SeChrome -ChromeOptions $chromeOptions
-}
-Catch {
-    Write-Host "Error starting web driver : $_"
-    return
-}
+$xamlFile = "C:\path\to\WebsiteTesterGUI.xaml"
+$stream = New-Object IO.FileStream $xamlFile, [System.IO.FileMode]::Open
+$xamlReader = New-Object System.Windows.Markup.XamlReader
+$window = $xamlReader.Load($stream)
 
-# Navigate website
-try {
-    $driver.Navigate().GoToUrl("https://www.example.com")
-}
-catch {
-    Write-Host "Error navigating website: $_"
-    $driver.Close()
-    return
-}
+# Get UI elements
+$txtUrl = $window.FindName("txtUrl")
+$btnTestWebsite = $window.FindName("btnTestWebsite")
 
-# Find an element on the page  
-try {
-    $searchBox = $driver.FindElementByName("search")
-}
-catch {
-    Write-Host "Error finding search box: $_"
-    $driver.Close()
-    return
-}
+# Event handler for "Test Website" button
+$btnTestWebsite.Add_Click({
+    # Get URL from input
+    $url = $txtUrl.Text
 
-# Enter search query
-try {
-    $searchBox.SendKeys("example")
-}
-catch {
-    Write-Host "Error entering search query: $_"
-    $driver.Close()
-    return
-}
+    # Import Selenium module
+    Import-Module Selenium
 
-# Submit search query
-try {
-    $searchBox.Submit()
-}
-catch {
-    Write-Host "Error submitting search query: $_"
-    $driver.Close()
-    return
-}
-
-# Wait for search results to load
-try {
-    $driver.WaitForElementByName("search-results", 10)
-}
-catch {
-    Write-Host "Error waiting for search results: $_"
-    $driver.Close()
-    return
-}
-
-# Check search results
-try{
-    $searchResults = $driver.FindElementByName("search-results")
-    if ($searchResults.Displayed) {
-        Write-Host "Search results found"
-    } else {
-        Write-Host "Search results not found"
+    # Start new web driver
+    try {
+        $chromeOptions = New-Object OpenQA.Selenium.Chrome.ChromeOptions
+        $chromeOptions.AcceptInsecureCertificates = $true
+        $driver = Start-SeChrome -ChromeOptions $chromeOptions
     }
-}
-catch {
-    Write-Host "Error checking search results: $_"
-}
+    Catch {
+        Write-Host "Error starting web driver : $_"
+        return
+    }
 
-# Close web driver
-$driver.Close()
+    # Navigate website
+    try {
+        $driver.Navigate().GoToUrl("https://www.example.com")
+    }
+    catch {
+        Write-Host "Error navigating website: $_"
+        $driver.Close()
+        return
+    }
+
+    # Find an element on the page  
+    try {
+        $searchBox = $driver.FindElementByName("search")
+    }
+    catch {
+        Write-Host "Error finding search box: $_"
+        $driver.Close()
+        return
+    }
+
+    # Enter search query
+    try {
+        $searchBox.SendKeys("example")
+    }
+    catch {
+        Write-Host "Error entering search query: $_"
+        $driver.Close()
+        return
+    }
+
+    # Submit search query
+    try {
+        $searchBox.Submit()
+    }
+    catch {
+        Write-Host "Error submitting search query: $_"
+        $driver.Close()
+        return
+    }
+
+    # Wait for search results to load
+    try {
+        $driver.WaitForElementByName("search-results", 10)
+    }
+    catch {
+        Write-Host "Error waiting for search results: $_"
+        $driver.Close()
+        return
+    }
+
+    # Check search results
+    try{
+        $searchResults = $driver.FindElementByName("search-results")
+        if ($searchResults.Displayed) {
+            Write-Host "Search results found"
+        } else {
+            Write-Host "Search results not found"
+        }
+    }
+    catch {
+        Write-Host "Error checking search results: $_"
+    }
+
+    # Close web driver
+    $driver.Close()
+
+})
+
+# Show GUI
+$window.ShowDialog() | Out-Null
